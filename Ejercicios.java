e17409a0cec13b5e6bbff2cd228ada388046d6c2
Rafo del Mal <rafo.reloaded@gmail.com>
2023-03-15 14:52:42 -0500

    Apilando en la Pila

---
+++ Ejercicios.java
@@ -0,0 +1,11 @@
+public class Ejercicios {
+
+    public static void main(String[] args) {
+        System.out.println("El programa Inicia");
+        Pila p = new Pila();
+        p.apilar("Primer Objeto");
+        p.apilar("Segundo Objeto");
+        System.out.println("El programa Finaliza");
+    }
+
+}
--- Pila.java
+++ Pila.java
@@ -2,4 +2,19 @@ public class Pila {
 
     private Object valor;
 
+    //tope
+    private Object cima;
+
+    private boolean estaVacia = true;
+
+    //push
+    public void apilar(Object objeto){
+
+    }
+
+    //pop
+    public void desapilar(){
+
+    }
+
 }
